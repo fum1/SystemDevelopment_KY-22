4096dbe02756e7ab7bc1a3462f3b57f838b697da
kawato0 <55867582+kawato0@users.noreply.github.com>
2019-10-10 13:43:49 +0900

    test

--- SystemDev_KY-22/SystemDev_KY-22/Program.cs
+++ SystemDev_KY-22/SystemDev_KY-22/Program.cs
@@ -16,7 +16,7 @@ namespace SystemDev_KY_22
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
