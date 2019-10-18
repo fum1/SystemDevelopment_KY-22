8b90e3aa7cd31164cd19a84adf922bcc970ae772
fumi <42640777+fum1@users.noreply.github.com>
2019-10-18 21:25:18 +0900

    進捗保存

--- SystemDev_KY-22/SystemDev_KY-22/SalesMenu.Designer.cs
+++ SystemDev_KY-22/SystemDev_KY-22/SalesMenu.Designer.cs
@@ -32,6 +32,7 @@
             this.PictB_Logo = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.btn_login = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.PictB_Logo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,9 +40,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(297, 276);
+            this.label1.Location = new System.Drawing.Point(559, 518);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(279, 38);
+            this.label1.Size = new System.Drawing.Size(440, 60);
             this.label1.TabIndex = 0;
             this.label1.Text = "営業担当メニュー";
             // 
@@ -49,9 +51,10 @@
             // 
             this.PictB_Logo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.PictB_Logo.Image = global::SystemDev_KY_22.Properties.Resources.チェリー;
-            this.PictB_Logo.Location = new System.Drawing.Point(75, 12);
+            this.PictB_Logo.Location = new System.Drawing.Point(153, 82);
+            this.PictB_Logo.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.PictB_Logo.Name = "PictB_Logo";
-            this.PictB_Logo.Size = new System.Drawing.Size(761, 222);
+            this.PictB_Logo.Size = new System.Drawing.Size(1274, 355);
             this.PictB_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PictB_Logo.TabIndex = 7;
             this.PictB_Logo.TabStop = false;
@@ -62,9 +65,10 @@
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(509, 353);
+            this.button1.Location = new System.Drawing.Point(1009, 687);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(316, 62);
+            this.button1.Size = new System.Drawing.Size(418, 99);
             this.button1.TabIndex = 8;
             this.button1.Text = "注文";
             this.button1.UseVisualStyleBackColor = false;
@@ -76,23 +80,40 @@
             this.btn_login.BackColor = System.Drawing.Color.Transparent;
             this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_login.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btn_login.Location = new System.Drawing.Point(96, 353);
+            this.btn_login.Location = new System.Drawing.Point(581, 687);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(316, 62);
+            this.btn_login.Size = new System.Drawing.Size(418, 99);
             this.btn_login.TabIndex = 9;
             this.btn_login.Text = "顧客登録";
             this.btn_login.UseVisualStyleBackColor = false;
             // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(153, 687);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(418, 99);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "注文";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // SalesMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(903, 450);
+            this.ClientSize = new System.Drawing.Size(1543, 842);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.btn_login);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.PictB_Logo);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "SalesMenu";
             this.Text = "Eigyoumenu";
             ((System.ComponentModel.ISupportInitialize)(this.PictB_Logo)).EndInit();
@@ -107,5 +128,6 @@
         private System.Windows.Forms.PictureBox PictB_Logo;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button button2;
     }
 }
