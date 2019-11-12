d1a0cc16f036419117df18a33aa8d5df1b5b1631
Keigo0427 <55825640+Keigo0427@users.noreply.github.com>
2019-11-12 17:49:14 +0900

    顧客登録の修正と少しコーディング

--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/ClientRegister.Designer.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/ClientRegister.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.groupBox = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_id = new System.Windows.Forms.Label();
             this.btn_login = new System.Windows.Forms.Button();
             this.txt_tel = new System.Windows.Forms.TextBox();
             this.lbl_tel = new System.Windows.Forms.Label();
@@ -41,12 +43,17 @@
             this.lbl_sex = new System.Windows.Forms.Label();
             this.rbtn_man = new System.Windows.Forms.RadioButton();
             this.txt_id = new System.Windows.Forms.TextBox();
-            this.txt_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox
             // 
+            this.groupBox.Controls.Add(this.textBox2);
+            this.groupBox.Controls.Add(this.label1);
+            this.groupBox.Controls.Add(this.textBox1);
+            this.groupBox.Controls.Add(this.lbl_id);
             this.groupBox.Controls.Add(this.btn_login);
             this.groupBox.Controls.Add(this.txt_tel);
             this.groupBox.Controls.Add(this.lbl_tel);
@@ -59,16 +66,35 @@
             this.groupBox.Controls.Add(this.lbl_sex);
             this.groupBox.Controls.Add(this.rbtn_man);
             this.groupBox.Controls.Add(this.txt_id);
-            this.groupBox.Controls.Add(this.txt_name);
             this.groupBox.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.groupBox.Location = new System.Drawing.Point(20, 20);
+            this.groupBox.Location = new System.Drawing.Point(22, 20);
             this.groupBox.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox.Name = "groupBox";
             this.groupBox.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox.Size = new System.Drawing.Size(1004, 645);
+            this.groupBox.Size = new System.Drawing.Size(1275, 673);
             this.groupBox.TabIndex = 14;
             this.groupBox.TabStop = false;
             this.groupBox.Text = "顧客情報";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(325, 61);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 54);
+            this.textBox1.TabIndex = 22;
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_id.Location = new System.Drawing.Point(183, 61);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(183, 48);
+            this.lbl_id.TabIndex = 21;
+            this.lbl_id.Text = "顧客ID：";
             // 
             // btn_login
             // 
@@ -76,7 +102,7 @@
             this.btn_login.BackColor = System.Drawing.Color.Transparent;
             this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_login.Font = new System.Drawing.Font("MS UI Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btn_login.Location = new System.Drawing.Point(441, 632);
+            this.btn_login.Location = new System.Drawing.Point(418, 564);
             this.btn_login.Margin = new System.Windows.Forms.Padding(4);
             this.btn_login.Name = "btn_login";
             this.btn_login.Size = new System.Drawing.Size(395, 74);
@@ -87,17 +113,17 @@
             // txt_tel
             // 
             this.txt_tel.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_tel.Location = new System.Drawing.Point(182, 449);
+            this.txt_tel.Location = new System.Drawing.Point(325, 482);
             this.txt_tel.Margin = new System.Windows.Forms.Padding(4);
             this.txt_tel.Name = "txt_tel";
-            this.txt_tel.Size = new System.Drawing.Size(774, 54);
+            this.txt_tel.Size = new System.Drawing.Size(497, 54);
             this.txt_tel.TabIndex = 18;
             // 
             // lbl_tel
             // 
             this.lbl_tel.AutoSize = true;
             this.lbl_tel.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_tel.Location = new System.Drawing.Point(40, 449);
+            this.lbl_tel.Location = new System.Drawing.Point(234, 482);
             this.lbl_tel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_tel.Name = "lbl_tel";
             this.lbl_tel.Size = new System.Drawing.Size(111, 48);
@@ -108,7 +134,7 @@
             // 
             this.lbl_address.AutoSize = true;
             this.lbl_address.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_address.Location = new System.Drawing.Point(29, 353);
+            this.lbl_address.Location = new System.Drawing.Point(223, 396);
             this.lbl_address.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_address.Name = "lbl_address";
             this.lbl_address.Size = new System.Drawing.Size(126, 48);
@@ -118,17 +144,18 @@
             // txt_address
             // 
             this.txt_address.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_address.Location = new System.Drawing.Point(188, 350);
+            this.txt_address.Location = new System.Drawing.Point(325, 395);
             this.txt_address.Margin = new System.Windows.Forms.Padding(4);
             this.txt_address.Name = "txt_address";
             this.txt_address.Size = new System.Drawing.Size(768, 54);
             this.txt_address.TabIndex = 15;
+            this.txt_address.TextChanged += new System.EventHandler(this.txt_address_TextChanged);
             // 
             // lbl_birthday
             // 
             this.lbl_birthday.AutoSize = true;
             this.lbl_birthday.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_birthday.Location = new System.Drawing.Point(8, 261);
+            this.lbl_birthday.Location = new System.Drawing.Point(586, 229);
             this.lbl_birthday.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_birthday.Name = "lbl_birthday";
             this.lbl_birthday.Size = new System.Drawing.Size(222, 48);
@@ -139,17 +166,17 @@
             // 
             this.dtp_birthday.CalendarFont = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.dtp_birthday.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.dtp_birthday.Location = new System.Drawing.Point(281, 263);
+            this.dtp_birthday.Location = new System.Drawing.Point(764, 229);
             this.dtp_birthday.Margin = new System.Windows.Forms.Padding(4);
             this.dtp_birthday.Name = "dtp_birthday";
-            this.dtp_birthday.Size = new System.Drawing.Size(675, 47);
+            this.dtp_birthday.Size = new System.Drawing.Size(308, 47);
             this.dtp_birthday.TabIndex = 13;
             // 
             // lbl_name
             // 
             this.lbl_name.AutoSize = true;
             this.lbl_name.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_name.Location = new System.Drawing.Point(8, 57);
+            this.lbl_name.Location = new System.Drawing.Point(223, 147);
             this.lbl_name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_name.Name = "lbl_name";
             this.lbl_name.Size = new System.Drawing.Size(126, 48);
@@ -160,7 +187,7 @@
             // 
             this.rbtn_woman.AutoSize = true;
             this.rbtn_woman.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.rbtn_woman.Location = new System.Drawing.Point(337, 168);
+            this.rbtn_woman.Location = new System.Drawing.Point(423, 231);
             this.rbtn_woman.Margin = new System.Windows.Forms.Padding(4);
             this.rbtn_woman.Name = "rbtn_woman";
             this.rbtn_woman.Size = new System.Drawing.Size(118, 44);
@@ -173,7 +200,7 @@
             // 
             this.lbl_sex.AutoSize = true;
             this.lbl_sex.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_sex.Location = new System.Drawing.Point(29, 163);
+            this.lbl_sex.Location = new System.Drawing.Point(224, 228);
             this.lbl_sex.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_sex.Name = "lbl_sex";
             this.lbl_sex.Size = new System.Drawing.Size(126, 48);
@@ -184,7 +211,7 @@
             // 
             this.rbtn_man.AutoSize = true;
             this.rbtn_man.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.rbtn_man.Location = new System.Drawing.Point(188, 168);
+            this.rbtn_man.Location = new System.Drawing.Point(327, 230);
             this.rbtn_man.Margin = new System.Windows.Forms.Padding(4);
             this.rbtn_man.Name = "rbtn_man";
             this.rbtn_man.Size = new System.Drawing.Size(118, 44);
@@ -196,20 +223,31 @@
             // txt_id
             // 
             this.txt_id.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_id.Location = new System.Drawing.Point(182, 68);
+            this.txt_id.Location = new System.Drawing.Point(326, 147);
             this.txt_id.Margin = new System.Windows.Forms.Padding(4);
             this.txt_id.Name = "txt_id";
             this.txt_id.Size = new System.Drawing.Size(383, 54);
             this.txt_id.TabIndex = 5;
             // 
-            // txt_name
+            // label1
             // 
-            this.txt_name.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_name.Location = new System.Drawing.Point(573, 68);
-            this.txt_name.Margin = new System.Windows.Forms.Padding(4);
-            this.txt_name.Name = "txt_name";
-            this.txt_name.Size = new System.Drawing.Size(383, 54);
-            this.txt_name.TabIndex = 8;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(142, 312);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 48);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "郵便番号：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(325, 312);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(383, 54);
+            this.textBox2.TabIndex = 24;
             // 
             // ClientRegister
             // 
@@ -217,7 +255,8 @@
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.groupBox);
             this.Name = "ClientRegister";
-            this.Size = new System.Drawing.Size(1042, 703);
+            this.Size = new System.Drawing.Size(1314, 768);
+            this.Load += new System.EventHandler(this.ClientRegister_Load);
             this.groupBox.ResumeLayout(false);
             this.groupBox.PerformLayout();
             this.ResumeLayout(false);
@@ -238,6 +277,9 @@
         private System.Windows.Forms.Label lbl_sex;
         private System.Windows.Forms.RadioButton rbtn_man;
         private System.Windows.Forms.TextBox txt_id;
-        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/ClientRegister.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/ClientRegister.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace SystemDev_KY_22
 {
     public partial class ClientRegister : UserControl
     {
+        OleDbConnection cn = new OleDbConnection();
         public ClientRegister()
         {
             InitializeComponent();
         }
+
+        private void ClientRegister_Load(object sender, EventArgs e)
+        {
+            cn.ConnectionString =
+                 @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\SysDev.accdb;";
+        }
+
+        private void txt_address_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
+
 }
