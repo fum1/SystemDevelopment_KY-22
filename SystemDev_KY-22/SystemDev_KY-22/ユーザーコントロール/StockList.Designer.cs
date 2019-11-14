ab6e0fa90f4c030f0e18afa468e00bc1dd2a7b6c
fum1 <b8032@oic.jp>
2019-11-14 15:28:23 +0900

    在庫一覧実装（一覧表示のみ

--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/StockList.Designer.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/StockList.Designer.cs
@@ -31,7 +31,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboB_CarName = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
@@ -42,28 +42,22 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboB_Maker = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.商品ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.商品名 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.定価 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.走行距離 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.色 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.実売価格 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.comboBox5);
-            this.panel1.Controls.Add(this.comboBox6);
+            this.panel1.Controls.Add(this.comboB_CarName);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.comboBox4);
@@ -74,7 +68,7 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.comboBox3);
-            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboB_Maker);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -87,9 +81,9 @@
             this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.checkBox1.Location = new System.Drawing.Point(859, 69);
+            this.checkBox1.Location = new System.Drawing.Point(891, 78);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(162, 49);
+            this.checkBox1.Size = new System.Drawing.Size(130, 40);
             this.checkBox1.TabIndex = 31;
             this.checkBox1.Text = "在庫なし";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -100,17 +94,17 @@
             this.comboBox5.FormattingEnabled = true;
             this.comboBox5.Location = new System.Drawing.Point(679, 75);
             this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 42);
+            this.comboBox5.Size = new System.Drawing.Size(121, 35);
             this.comboBox5.TabIndex = 28;
             // 
-            // comboBox6
+            // comboB_CarName
             // 
-            this.comboBox6.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Location = new System.Drawing.Point(180, 27);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(121, 42);
-            this.comboBox6.TabIndex = 29;
+            this.comboB_CarName.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboB_CarName.FormattingEnabled = true;
+            this.comboB_CarName.Location = new System.Drawing.Point(180, 27);
+            this.comboB_CarName.Name = "comboB_CarName";
+            this.comboB_CarName.Size = new System.Drawing.Size(121, 35);
+            this.comboB_CarName.TabIndex = 29;
             // 
             // label7
             // 
@@ -118,7 +112,7 @@
             this.label7.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label7.Location = new System.Drawing.Point(613, 80);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(43, 30);
+            this.label7.Size = new System.Drawing.Size(34, 24);
             this.label7.TabIndex = 27;
             this.label7.Text = "～";
             // 
@@ -128,7 +122,7 @@
             this.label8.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label8.Location = new System.Drawing.Point(68, 29);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(80, 45);
+            this.label8.Size = new System.Drawing.Size(63, 36);
             this.label8.TabIndex = 30;
             this.label8.Text = "車名";
             // 
@@ -138,7 +132,7 @@
             this.comboBox4.FormattingEnabled = true;
             this.comboBox4.Location = new System.Drawing.Point(461, 74);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 42);
+            this.comboBox4.Size = new System.Drawing.Size(121, 35);
             this.comboBox4.TabIndex = 26;
             // 
             // button1
@@ -160,7 +154,7 @@
             this.label6.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label6.Location = new System.Drawing.Point(327, 76);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(140, 45);
+            this.label6.Size = new System.Drawing.Size(111, 36);
             this.label6.TabIndex = 25;
             this.label6.Text = "走行距離";
             // 
@@ -183,7 +177,7 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(679, 28);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 42);
+            this.comboBox1.Size = new System.Drawing.Size(121, 35);
             this.comboBox1.TabIndex = 24;
             // 
             // label3
@@ -192,7 +186,7 @@
             this.label3.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label3.Location = new System.Drawing.Point(34, 71);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(140, 45);
+            this.label3.Size = new System.Drawing.Size(111, 36);
             this.label3.TabIndex = 13;
             this.label3.Text = "メーカー";
             // 
@@ -202,7 +196,7 @@
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.Location = new System.Drawing.Point(613, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 30);
+            this.label1.Size = new System.Drawing.Size(34, 24);
             this.label1.TabIndex = 20;
             this.label1.Text = "～";
             // 
@@ -212,17 +206,17 @@
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Location = new System.Drawing.Point(461, 27);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 42);
+            this.comboBox3.Size = new System.Drawing.Size(121, 35);
             this.comboBox3.TabIndex = 23;
             // 
-            // comboBox2
+            // comboB_Maker
             // 
-            this.comboBox2.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(180, 74);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 42);
-            this.comboBox2.TabIndex = 22;
+            this.comboB_Maker.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboB_Maker.FormattingEnabled = true;
+            this.comboB_Maker.Location = new System.Drawing.Point(180, 74);
+            this.comboB_Maker.Name = "comboB_Maker";
+            this.comboB_Maker.Size = new System.Drawing.Size(121, 35);
+            this.comboB_Maker.TabIndex = 22;
             // 
             // label5
             // 
@@ -230,7 +224,7 @@
             this.label5.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label5.Location = new System.Drawing.Point(348, 28);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(80, 45);
+            this.label5.Size = new System.Drawing.Size(63, 36);
             this.label5.TabIndex = 15;
             this.label5.Text = "価格";
             // 
@@ -258,59 +252,22 @@
             this.flowLayoutPanel2.Size = new System.Drawing.Size(19, 527);
             this.flowLayoutPanel2.TabIndex = 4;
             // 
-            // dataGridView2
+            // dataGridView1
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.商品ID,
-            this.商品名,
-            this.定価,
-            this.走行距離,
-            this.色,
-            this.実売価格});
-            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView2.Location = new System.Drawing.Point(19, 149);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowTemplate.Height = 21;
-            this.dataGridView2.Size = new System.Drawing.Size(1002, 527);
-            this.dataGridView2.TabIndex = 5;
-            // 
-            // 商品ID
-            // 
-            this.商品ID.HeaderText = "商品ID";
-            this.商品ID.Name = "商品ID";
-            // 
-            // 商品名
-            // 
-            this.商品名.HeaderText = "商品名";
-            this.商品名.Name = "商品名";
-            // 
-            // 定価
-            // 
-            this.定価.HeaderText = "価格";
-            this.定価.Name = "定価";
-            // 
-            // 走行距離
-            // 
-            this.走行距離.HeaderText = "走行距離";
-            this.走行距離.Name = "走行距離";
-            // 
-            // 色
-            // 
-            this.色.HeaderText = "色";
-            this.色.Name = "色";
-            // 
-            // 実売価格
-            // 
-            this.実売価格.HeaderText = "実売価格";
-            this.実売価格.Name = "実売価格";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 149);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(1002, 527);
+            this.dataGridView1.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(109, 202);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.Size = new System.Drawing.Size(0, 12);
             this.label4.TabIndex = 14;
             // 
             // StockList
@@ -318,16 +275,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.panel1);
             this.Name = "StockList";
             this.Size = new System.Drawing.Size(1042, 703);
+            this.Load += new System.EventHandler(this.StockList_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -338,28 +296,22 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
-        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboB_Maker;
         private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboB_CarName;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn 商品ID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn 商品名;
-        private System.Windows.Forms.DataGridViewTextBoxColumn 定価;
-        private System.Windows.Forms.DataGridViewTextBoxColumn 走行距離;
-        private System.Windows.Forms.DataGridViewTextBoxColumn 色;
-        private System.Windows.Forms.DataGridViewTextBoxColumn 実売価格;
     }
 }
