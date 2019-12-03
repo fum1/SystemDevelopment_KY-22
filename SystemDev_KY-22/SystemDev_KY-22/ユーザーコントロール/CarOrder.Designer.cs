c76f94643d9b940be4e20f44afe5616f68c03d41
kawato0 <55867582+kawato0@users.noreply.github.com>
2019-12-03 17:52:47 +0900

    発注画面途中

--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/CarOrder.Designer.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/CarOrder.Designer.cs
@@ -31,7 +31,6 @@
             this.lbl_supplier = new System.Windows.Forms.Label();
             this.cmb_supplier = new System.Windows.Forms.ComboBox();
             this.lbl_Person = new System.Windows.Forms.Label();
-            this.cmb_Person = new System.Windows.Forms.ComboBox();
             this.lbl_orderdate = new System.Windows.Forms.Label();
             this.lbl_productname = new System.Windows.Forms.Label();
             this.txt_productname = new System.Windows.Forms.TextBox();
@@ -45,6 +44,13 @@
             this.txt_color = new System.Windows.Forms.TextBox();
             this.txt_option = new System.Windows.Forms.TextBox();
             this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.btn_Check = new System.Windows.Forms.Button();
+            this.btn_preview = new System.Windows.Forms.Button();
+            this.lbl_orderID = new System.Windows.Forms.Label();
+            this.txt_orderID = new System.Windows.Forms.TextBox();
+            this.txt_Person = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,18 +58,18 @@
             // 
             this.lbl_supplier.AutoSize = true;
             this.lbl_supplier.Font = new System.Drawing.Font("メイリオ", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_supplier.Location = new System.Drawing.Point(29, 61);
+            this.lbl_supplier.Location = new System.Drawing.Point(42, 81);
             this.lbl_supplier.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lbl_supplier.Name = "lbl_supplier";
-            this.lbl_supplier.Size = new System.Drawing.Size(139, 57);
+            this.lbl_supplier.Size = new System.Drawing.Size(182, 57);
             this.lbl_supplier.TabIndex = 0;
-            this.lbl_supplier.Text = "仕入先";
+            this.lbl_supplier.Text = "仕入先ID";
             // 
             // cmb_supplier
             // 
             this.cmb_supplier.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.cmb_supplier.FormattingEnabled = true;
-            this.cmb_supplier.Location = new System.Drawing.Point(278, 61);
+            this.cmb_supplier.Location = new System.Drawing.Point(291, 81);
             this.cmb_supplier.Margin = new System.Windows.Forms.Padding(5);
             this.cmb_supplier.Name = "cmb_supplier";
             this.cmb_supplier.Size = new System.Drawing.Size(233, 42);
@@ -73,39 +79,29 @@
             // 
             this.lbl_Person.AutoSize = true;
             this.lbl_Person.Font = new System.Drawing.Font("メイリオ", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_Person.Location = new System.Drawing.Point(29, 132);
+            this.lbl_Person.Location = new System.Drawing.Point(42, 152);
             this.lbl_Person.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lbl_Person.Name = "lbl_Person";
-            this.lbl_Person.Size = new System.Drawing.Size(139, 57);
+            this.lbl_Person.Size = new System.Drawing.Size(144, 57);
             this.lbl_Person.TabIndex = 12;
-            this.lbl_Person.Text = "担当者";
-            // 
-            // cmb_Person
-            // 
-            this.cmb_Person.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.cmb_Person.FormattingEnabled = true;
-            this.cmb_Person.Location = new System.Drawing.Point(278, 132);
-            this.cmb_Person.Margin = new System.Windows.Forms.Padding(5);
-            this.cmb_Person.Name = "cmb_Person";
-            this.cmb_Person.Size = new System.Drawing.Size(233, 42);
-            this.cmb_Person.TabIndex = 13;
+            this.lbl_Person.Text = "社員ID";
             // 
             // lbl_orderdate
             // 
             this.lbl_orderdate.AutoSize = true;
             this.lbl_orderdate.Font = new System.Drawing.Font("メイリオ", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_orderdate.Location = new System.Drawing.Point(29, 203);
+            this.lbl_orderdate.Location = new System.Drawing.Point(42, 223);
             this.lbl_orderdate.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lbl_orderdate.Name = "lbl_orderdate";
-            this.lbl_orderdate.Size = new System.Drawing.Size(139, 57);
+            this.lbl_orderdate.Size = new System.Drawing.Size(215, 57);
             this.lbl_orderdate.TabIndex = 14;
-            this.lbl_orderdate.Text = "発注日";
+            this.lbl_orderdate.Text = "発注年月日";
             // 
             // lbl_productname
             // 
             this.lbl_productname.AutoSize = true;
             this.lbl_productname.Font = new System.Drawing.Font("メイリオ", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_productname.Location = new System.Drawing.Point(29, 274);
+            this.lbl_productname.Location = new System.Drawing.Point(42, 294);
             this.lbl_productname.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lbl_productname.Name = "lbl_productname";
             this.lbl_productname.Size = new System.Drawing.Size(139, 57);
@@ -115,7 +111,7 @@
             // txt_productname
             // 
             this.txt_productname.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_productname.Location = new System.Drawing.Point(278, 274);
+            this.txt_productname.Location = new System.Drawing.Point(291, 294);
             this.txt_productname.Margin = new System.Windows.Forms.Padding(5);
             this.txt_productname.Name = "txt_productname";
             this.txt_productname.Size = new System.Drawing.Size(233, 42);
@@ -125,7 +121,7 @@
             // 
             this.dtp_admissionday.Font = new System.Drawing.Font("メイリオ", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.dtp_admissionday.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtp_admissionday.Location = new System.Drawing.Point(278, 203);
+            this.dtp_admissionday.Location = new System.Drawing.Point(291, 223);
             this.dtp_admissionday.Margin = new System.Windows.Forms.Padding(4);
             this.dtp_admissionday.Name = "dtp_admissionday";
             this.dtp_admissionday.Size = new System.Drawing.Size(233, 48);
@@ -136,30 +132,32 @@
             this.btn_clear.BackColor = System.Drawing.Color.Transparent;
             this.btn_clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_clear.Font = new System.Drawing.Font("メイリオ", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btn_clear.Location = new System.Drawing.Point(205, 567);
+            this.btn_clear.Location = new System.Drawing.Point(361, 567);
             this.btn_clear.Name = "btn_clear";
             this.btn_clear.Size = new System.Drawing.Size(150, 56);
             this.btn_clear.TabIndex = 25;
             this.btn_clear.Text = "クリア";
             this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
             // 
             // btn_order
             // 
             this.btn_order.BackColor = System.Drawing.Color.Transparent;
             this.btn_order.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_order.Font = new System.Drawing.Font("メイリオ", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btn_order.Location = new System.Drawing.Point(361, 567);
+            this.btn_order.Location = new System.Drawing.Point(205, 567);
             this.btn_order.Name = "btn_order";
             this.btn_order.Size = new System.Drawing.Size(150, 56);
             this.btn_order.TabIndex = 26;
             this.btn_order.Text = "発注";
             this.btn_order.UseVisualStyleBackColor = false;
+            this.btn_order.Click += new System.EventHandler(this.btn_order_Click);
             // 
             // lbl_color
             // 
             this.lbl_color.AutoSize = true;
             this.lbl_color.Font = new System.Drawing.Font("メイリオ", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_color.Location = new System.Drawing.Point(29, 416);
+            this.lbl_color.Location = new System.Drawing.Point(42, 436);
             this.lbl_color.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lbl_color.Name = "lbl_color";
             this.lbl_color.Size = new System.Drawing.Size(63, 57);
@@ -170,7 +168,7 @@
             // 
             this.lbl_model.AutoSize = true;
             this.lbl_model.Font = new System.Drawing.Font("メイリオ", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_model.Location = new System.Drawing.Point(29, 345);
+            this.lbl_model.Location = new System.Drawing.Point(42, 365);
             this.lbl_model.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lbl_model.Name = "lbl_model";
             this.lbl_model.Size = new System.Drawing.Size(139, 57);
@@ -181,7 +179,7 @@
             // 
             this.lbl_option.AutoSize = true;
             this.lbl_option.Font = new System.Drawing.Font("メイリオ", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.lbl_option.Location = new System.Drawing.Point(29, 487);
+            this.lbl_option.Location = new System.Drawing.Point(42, 507);
             this.lbl_option.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lbl_option.Name = "lbl_option";
             this.lbl_option.Size = new System.Drawing.Size(215, 57);
@@ -191,7 +189,7 @@
             // txt_model
             // 
             this.txt_model.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_model.Location = new System.Drawing.Point(278, 345);
+            this.txt_model.Location = new System.Drawing.Point(291, 365);
             this.txt_model.Margin = new System.Windows.Forms.Padding(5);
             this.txt_model.Name = "txt_model";
             this.txt_model.Size = new System.Drawing.Size(233, 42);
@@ -200,7 +198,7 @@
             // txt_color
             // 
             this.txt_color.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_color.Location = new System.Drawing.Point(278, 416);
+            this.txt_color.Location = new System.Drawing.Point(291, 436);
             this.txt_color.Margin = new System.Windows.Forms.Padding(5);
             this.txt_color.Name = "txt_color";
             this.txt_color.Size = new System.Drawing.Size(233, 42);
@@ -209,7 +207,7 @@
             // txt_option
             // 
             this.txt_option.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.txt_option.Location = new System.Drawing.Point(278, 487);
+            this.txt_option.Location = new System.Drawing.Point(291, 507);
             this.txt_option.Margin = new System.Windows.Forms.Padding(5);
             this.txt_option.Name = "txt_option";
             this.txt_option.Size = new System.Drawing.Size(233, 42);
@@ -223,15 +221,92 @@
             this.pictureBox.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox.Name = "pictureBox";
             this.pictureBox.Size = new System.Drawing.Size(567, 608);
-            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox.TabIndex = 38;
             this.pictureBox.TabStop = false;
+            // 
+            // btn_print
+            // 
+            this.btn_print.BackColor = System.Drawing.Color.Transparent;
+            this.btn_print.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_print.Font = new System.Drawing.Font("メイリオ", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_print.Location = new System.Drawing.Point(517, 629);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(150, 56);
+            this.btn_print.TabIndex = 39;
+            this.btn_print.Text = "印刷";
+            this.btn_print.UseVisualStyleBackColor = false;
+            this.btn_print.Click += new System.EventHandler(this.btn_print_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // btn_Check
+            // 
+            this.btn_Check.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Check.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Check.Font = new System.Drawing.Font("メイリオ", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_Check.Location = new System.Drawing.Point(205, 629);
+            this.btn_Check.Name = "btn_Check";
+            this.btn_Check.Size = new System.Drawing.Size(150, 56);
+            this.btn_Check.TabIndex = 40;
+            this.btn_Check.Text = "表示";
+            this.btn_Check.UseVisualStyleBackColor = false;
+            this.btn_Check.Click += new System.EventHandler(this.btn_Check_Click);
+            // 
+            // btn_preview
+            // 
+            this.btn_preview.BackColor = System.Drawing.Color.Transparent;
+            this.btn_preview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_preview.Font = new System.Drawing.Font("メイリオ", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_preview.Location = new System.Drawing.Point(361, 629);
+            this.btn_preview.Name = "btn_preview";
+            this.btn_preview.Size = new System.Drawing.Size(150, 56);
+            this.btn_preview.TabIndex = 41;
+            this.btn_preview.Text = "プレ";
+            this.btn_preview.UseVisualStyleBackColor = false;
+            this.btn_preview.Click += new System.EventHandler(this.btn_preview_Click);
+            // 
+            // lbl_orderID
+            // 
+            this.lbl_orderID.AutoSize = true;
+            this.lbl_orderID.Font = new System.Drawing.Font("メイリオ", 22.2F);
+            this.lbl_orderID.Location = new System.Drawing.Point(42, 15);
+            this.lbl_orderID.Name = "lbl_orderID";
+            this.lbl_orderID.Size = new System.Drawing.Size(144, 57);
+            this.lbl_orderID.TabIndex = 42;
+            this.lbl_orderID.Text = "発注ID";
+            // 
+            // txt_orderID
+            // 
+            this.txt_orderID.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_orderID.Location = new System.Drawing.Point(291, 15);
+            this.txt_orderID.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_orderID.Name = "txt_orderID";
+            this.txt_orderID.Size = new System.Drawing.Size(233, 42);
+            this.txt_orderID.TabIndex = 43;
+            // 
+            // txt_Person
+            // 
+            this.txt_Person.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_Person.Location = new System.Drawing.Point(291, 152);
+            this.txt_Person.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_Person.Name = "txt_Person";
+            this.txt_Person.Size = new System.Drawing.Size(233, 42);
+            this.txt_Person.TabIndex = 44;
             // 
             // CarOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.txt_Person);
+            this.Controls.Add(this.txt_orderID);
+            this.Controls.Add(this.lbl_orderID);
+            this.Controls.Add(this.btn_preview);
+            this.Controls.Add(this.btn_Check);
+            this.Controls.Add(this.btn_print);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.txt_option);
             this.Controls.Add(this.txt_color);
@@ -245,7 +320,6 @@
             this.Controls.Add(this.txt_productname);
             this.Controls.Add(this.lbl_productname);
             this.Controls.Add(this.lbl_orderdate);
-            this.Controls.Add(this.cmb_Person);
             this.Controls.Add(this.lbl_Person);
             this.Controls.Add(this.cmb_supplier);
             this.Controls.Add(this.lbl_supplier);
@@ -265,7 +339,6 @@
         private System.Windows.Forms.Label lbl_supplier;
         private System.Windows.Forms.ComboBox cmb_supplier;
         private System.Windows.Forms.Label lbl_Person;
-        private System.Windows.Forms.ComboBox cmb_Person;
         private System.Windows.Forms.Label lbl_orderdate;
         private System.Windows.Forms.Label lbl_productname;
         private System.Windows.Forms.TextBox txt_productname;
@@ -279,5 +352,12 @@
         private System.Windows.Forms.TextBox txt_color;
         private System.Windows.Forms.TextBox txt_option;
         private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btn_print;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button btn_Check;
+        private System.Windows.Forms.Button btn_preview;
+        private System.Windows.Forms.Label lbl_orderID;
+        private System.Windows.Forms.TextBox txt_orderID;
+        private System.Windows.Forms.TextBox txt_Person;
     }
 }
--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/CarOrder.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/CarOrder.cs
@@ -7,19 +7,145 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Drawing.Printing;
 
 namespace SystemDev_KY_22
 {
     public partial class CarOrder : UserControl
     {
+        OleDbConnection cn = new OleDbConnection();  //コネクションオブジェクト
         public CarOrder()
         {
+
             InitializeComponent();
         }
 
         private void CarOrder_Load(object sender, EventArgs e)
         {
+            cn.ConnectionString =
+                @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\SysDev.accdb;";
 
+        }
+
+        private void btn_print_Click(object sender, EventArgs e)
+        {
+
+            PrintDocument pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            pd.Print();
+        }
+
+
+
+        private void btn_preview_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Printing.PrintDocument pd = new System.Drawing.Printing.PrintDocument();
+            pd.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(printDocument1_PrintPage);
+            PrintPreviewDialog ppd = new PrintPreviewDialog();
+            ppd.Document = pd;
+            ppd.ShowDialog();
+        }
+
+        private void btn_Check_Click(object sender, EventArgs e)
+        {
+            //表示する文字列
+            string print_lblsupplier = lbl_supplier.Text;
+            string print_lblPerson = lbl_Person.Text;
+            string print_lblorderdate = lbl_orderdate.Text;
+            string print_lblproductname = lbl_productname.Text;
+            string print_lblmodel = lbl_model.Text;
+            string print_lblcolor = lbl_color.Text;
+            string print_lbloption = lbl_option.Text;
+            string print_cmbsupplier = cmb_supplier.Text;
+            string print_cmbPerson = txt_Person.Text;
+            string print_cmborderdate = dtp_admissionday.Text;
+            string print_cmbproductname = txt_productname.Text;
+            string print_cmbmodel = txt_model.Text;
+            string print_cmbcolor = txt_color.Text;
+            string print_cmboption = txt_option.Text;
+
+            Bitmap canvas = new Bitmap(pictureBox.Width, pictureBox.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            Font fnt = new Font("HGP明朝E", 20);
+            StringFormat sf = new StringFormat();
+            //文字列を描画するときの大きさを計測する
+            SizeF stringSize = g.MeasureString(print_lblsupplier, fnt, 500, sf);
+
+            //文字列を描画する
+
+            g.DrawString(print_lblsupplier + "     " + print_cmbsupplier, fnt, Brushes.Black, 0, 0, sf);
+            g.DrawString(print_lblPerson + "     " + print_cmbPerson, fnt, Brushes.Black, 0, 35, sf);
+            g.DrawString(print_lblorderdate + "     " + print_cmborderdate, fnt, Brushes.Black, 0, 70, sf);
+            g.DrawString(print_lblproductname + "     " + print_cmbproductname, fnt, Brushes.Black, 0, 105, sf);
+            g.DrawString(print_lblmodel + "     " + print_cmbmodel, fnt, Brushes.Black, 0, 140, sf);
+            g.DrawString(print_lblcolor + "           " + print_cmbcolor, fnt, Brushes.Black, 0, 175, sf);
+            g.DrawString(print_lbloption + " " + print_cmboption, fnt, Brushes.Black, 0, 210, sf);
+
+
+
+            //リソースを解放する
+            fnt.Dispose();
+            sf.Dispose();
+            g.Dispose();
+
+            //PictureBox1に表示する
+            pictureBox.Image = canvas;
+        }
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            var image = pictureBox.Image;
+            e.Graphics.DrawImage(image, e.MarginBounds);
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            cmb_supplier.ResetText();
+            txt_Person.ResetText();
+            dtp_admissionday.ResetText();
+            txt_productname.ResetText();
+            txt_model.ResetText();
+            txt_color.ResetText();
+            txt_option.ResetText();
+
+        }
+
+        private void btn_order_Click(object sender, EventArgs e)
+        {
+            OleDbCommand cmd =
+                new OleDbCommand("INSERT INTO 発注テーブル (発注ID,発注年月日, 社員ID, 仕入先ID, 商品名,モデル,色,オプション) " +
+                "VALUES (@発注ID, @発注年月日, @社員ID, @仕入先ID, @商品名,@モデル,@色,@オプション)", cn);
+            //DBの列名に、PassWord (Microsoft Jet 4.0 の予約語)は使用できない
+            //@パラメータが出てくる順番に設定する
+            cmd.Parameters.AddWithValue("@発注ID", txt_orderID);
+            cmd.Parameters.AddWithValue("@発注年月日", dtp_admissionday.Text);                 //IDのデータ
+            cmd.Parameters.AddWithValue("@社員ID", txt_Person.Text);             //Passのデータ
+            cmd.Parameters.AddWithValue("@仕入先ID", dtp_admissionday.Text);             //Nameのデータ
+            cmd.Parameters.AddWithValue("@商品名", txt_productname.Text);   //PostNumberのデータ
+            cmd.Parameters.AddWithValue("@モデル", txt_model.Text);      //Addressのデータ
+            cmd.Parameters.AddWithValue("@色", txt_color.Text);
+            cmd.Parameters.AddWithValue("@オプション", txt_option.Text);
+           
+            try
+            {
+                cn.Open();                 //コネクションを開く
+                cmd.ExecuteNonQuery();     //SQLを実行
+                cn.Close();                //コネクションを閉じる
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cn.Close();               //コネクションを閉じる
+                return;
+            }
+
+
+            MessageBox.Show("登録しました", "管理者");
+
+            //発注登録途中　完成まだ
         }
     }
 }
+
