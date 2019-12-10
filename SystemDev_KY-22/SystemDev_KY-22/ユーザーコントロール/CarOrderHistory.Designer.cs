a62d0a0c9f81ab1938bafee27df14a617dd215ac
maru8375 <55867559+maru8375@users.noreply.github.com>
2019-12-10 17:53:24 +0900

    発注履歴　デザイン完了・コード途中

---
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/CarOrderHistory.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SystemDev_KY_22.ユーザーコントロール
+{
+    partial class CarOrderHistory
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cmd_Supplier = new System.Windows.Forms.ComboBox();
+            this.cmd_Product = new System.Windows.Forms.ComboBox();
+            this.cmd_Emp = new System.Windows.Forms.ComboBox();
+            this.btn_Seach = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1230, 28);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 714);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1230, 21);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 28);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(26, 686);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel4.Location = new System.Drawing.Point(1201, 28);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(29, 686);
+            this.panel4.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 157);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1163, 551);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(60, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "年月日";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(32, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 36);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "仕入先ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(427, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 36);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "商品ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(427, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 36);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "社員ID";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dateTimePicker1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dateTimePicker1.Location = new System.Drawing.Point(159, 42);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(227, 42);
+            this.dateTimePicker1.TabIndex = 9;
+            this.dateTimePicker1.Value = new System.DateTime(2019, 12, 10, 0, 0, 0, 0);
+            // 
+            // cmd_Supplier
+            // 
+            this.cmd_Supplier.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmd_Supplier.FormattingEnabled = true;
+            this.cmd_Supplier.Location = new System.Drawing.Point(159, 98);
+            this.cmd_Supplier.Name = "cmd_Supplier";
+            this.cmd_Supplier.Size = new System.Drawing.Size(185, 42);
+            this.cmd_Supplier.TabIndex = 10;
+            // 
+            // cmd_Product
+            // 
+            this.cmd_Product.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmd_Product.FormattingEnabled = true;
+            this.cmd_Product.Location = new System.Drawing.Point(524, 42);
+            this.cmd_Product.Name = "cmd_Product";
+            this.cmd_Product.Size = new System.Drawing.Size(185, 42);
+            this.cmd_Product.TabIndex = 11;
+            // 
+            // cmd_Emp
+            // 
+            this.cmd_Emp.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmd_Emp.FormattingEnabled = true;
+            this.cmd_Emp.Location = new System.Drawing.Point(524, 98);
+            this.cmd_Emp.Name = "cmd_Emp";
+            this.cmd_Emp.Size = new System.Drawing.Size(185, 42);
+            this.cmd_Emp.TabIndex = 12;
+            // 
+            // btn_Seach
+            // 
+            this.btn_Seach.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Seach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Seach.Font = new System.Drawing.Font("メイリオ", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_Seach.Location = new System.Drawing.Point(1023, 88);
+            this.btn_Seach.Name = "btn_Seach";
+            this.btn_Seach.Size = new System.Drawing.Size(172, 56);
+            this.btn_Seach.TabIndex = 27;
+            this.btn_Seach.Text = "検索";
+            this.btn_Seach.UseVisualStyleBackColor = false;
+            this.btn_Seach.Click += new System.EventHandler(this.btn_Seach_Click);
+            // 
+            // CarOrderHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_Seach);
+            this.Controls.Add(this.cmd_Emp);
+            this.Controls.Add(this.cmd_Product);
+            this.Controls.Add(this.cmd_Supplier);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "CarOrderHistory";
+            this.Size = new System.Drawing.Size(1230, 735);
+            this.Load += new System.EventHandler(this.CarOrderHistory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cmd_Supplier;
+        private System.Windows.Forms.ComboBox cmd_Product;
+        private System.Windows.Forms.ComboBox cmd_Emp;
+        private System.Windows.Forms.Button btn_Seach;
+    }
+}
