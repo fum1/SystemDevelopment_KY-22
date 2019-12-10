6ccf991efaa572e786e3046c85d4066e2df1a074
gyuhu <55867587+gyuhu@users.noreply.github.com>
2019-12-10 17:54:51 +0900

    仕入先一覧作成途中

--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/Vendor list.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/Vendor list.cs
@@ -14,6 +14,7 @@ namespace SystemDev_KY_22.ユーザーコントロール
     public partial class Vendor_list : UserControl
     {
         OleDbConnection cn = new OleDbConnection();  //コネクションオブジェクト
+        DataTable dt = new DataTable();
         public Vendor_list()
         {
             InitializeComponent();
@@ -24,64 +25,6 @@ namespace SystemDev_KY_22.ユーザーコントロール
             cn.ConnectionString =
                  @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\SysDev.accdb;";
 
-            dataload();
-
-        }
-
-        private void dataload()   //カスタム関数
-        {
-
-            OleDbDataAdapter da =
-                new OleDbDataAdapter("SELECT 仕入先ID , 仕入先会社名 ,仕入先郵便番号 , 仕入先住所 ," +
-                "仕入先電話番号 , 商品ID ," +
-                "FROM 仕入先マスタ ORDER BY 仕入先ID", cn);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-
-
-            dgv_vendorlist.DataSource = dt;
-            dgv_vendorlist.AllowUserToAddRows = false;    //最下行を非表示
-            dgv_vendorlist.AutoResizeColumns();           //列の幅の自動調整
-
-            cmb_SupplierID.DataSource = dt;
-            cmb_SupplierID.DisplayMember = "仕入先ID";
-            cmb_SupplierID.ValueMember = "仕入先ID";
-
-            cmb_Suppliername.DataSource = dt;
-            cmb_Suppliername.DisplayMember = "仕入先会社名";
-            cmb_Suppliername.ValueMember = "仕入先会社名";
-
-            cmb_postalcode.DataSource = dt;
-            cmb_postalcode.DisplayMember = "仕入先郵便番号";
-            cmb_postalcode.ValueMember = "仕入先郵便番号";
-
-            cmb_address.DataSource = dt;
-            cmb_address.DisplayMember = "仕入先住所";
-            cmb_address.ValueMember = "仕入先住所";
-
-            cmb_tel.DataSource = dt;
-            cmb_tel.DisplayMember = "仕入先電話番号";
-            cmb_tel.ValueMember = "仕入先電話番号";
-
-            cmb_productID.DataSource = dt;
-            cmb_productID.DisplayMember = "商品ID";
-            cmb_productID.ValueMember = "商品ID";
-
-
-
-
-            OleDbDataAdapter　product_da =
-                new OleDbDataAdapter("SELECT 商品ID,商品名,定価,車種ID,メーカーID" +
-                " FROM 商品マスタ", cn);
-            DataTable product_dt = new DataTable();
-            product_da.Fill(product_dt);
-
-            cmb_productID.DataSource = product_dt;
-            cmb_productID.DisplayMember = "商品ID";
-            cmb_productID.ValueMember = "商品ID";
-
-
-          
 
 
         }
@@ -89,54 +32,51 @@ namespace SystemDev_KY_22.ユーザーコントロール
         private void btn_Suppliersearch_Click(object sender, EventArgs e)
         {
             OleDbCommand cmd =
-               new OleDbCommand("SELECT 仕入先ID , 仕入先会社名 ,仕入先郵便番号 , 仕入先住所 ," +
-                "仕入先電話番号 , 商品ID ," +
-                "FROM 仕入先マスタ WHERE 仕入先ID = @仕入先ID  ORDER BY 仕入先ID");
+               new OleDbCommand("SELECT 仕入先ID ,商品ID ,仕入先会社名 ,仕入先郵便番号 ,仕入先住所 ,仕入先電話番号  " +
+              "FROM 仕入先マスタ WHERE 仕入先ID = @仕入先ID  ORDER BY 仕入先ID");
             cmd.Connection = cn;
             OleDbDataAdapter da = new OleDbDataAdapter();
             da.SelectCommand = cmd;
 
-            cmd.Parameters.AddWithValue("@仕入先ID", cmb_SupplierID.Text);
+            cmd.Parameters.AddWithValue("@仕入先ID", txt_SupplierID.Text);
 
 
             DataTable dt = new DataTable();
             da.Fill(dt);
             dgv_vendorlist.DataSource = dt;
             dgv_vendorlist.AllowUserToAddRows = false;   //最下行を非表示
-            dgv_vendorlist.AutoResizeColumns();          //列の幅の自動調整
+            dgv_vendorlist.AutoResizeColumns();          //列の幅の自動
         }
 
         private void btn_Suppliernamesearch_Click(object sender, EventArgs e)
         {
             OleDbCommand cmd =
-               new OleDbCommand("SELECT 仕入先ID , 仕入先会社名 ,仕入先郵便番号 , 仕入先住所 ," +
-                "仕入先電話番号 , 商品ID ," +
-                "FROM 仕入先マスタ WHERE 仕入先会社名 = @仕入先会社名  ORDER BY 仕入先会社名");
+              new OleDbCommand(("SELECT 仕入先ID ,商品ID ,仕入先会社名 ,仕入先郵便番号 ,仕入先住所 ,仕入先電話番号 " +
+              "FROM 仕入先マスタ WHERE 仕入先会社名 = @仕入先会社名  ORDER BY 仕入先会社名"));
             cmd.Connection = cn;
             OleDbDataAdapter da = new OleDbDataAdapter();
             da.SelectCommand = cmd;
 
-            cmd.Parameters.AddWithValue("@仕入先会社名", cmb_Suppliername.Text);
+            cmd.Parameters.AddWithValue("@仕入先会社名", txt_Suppliername.Text);
 
 
             DataTable dt = new DataTable();
             da.Fill(dt);
             dgv_vendorlist.DataSource = dt;
             dgv_vendorlist.AllowUserToAddRows = false;   //最下行を非表示
-            dgv_vendorlist.AutoResizeColumns();          //列の幅の自動調整
+            dgv_vendorlist.AutoResizeColumns();          //列の幅の自動調
         }
 
         private void btn_postalcodesearch_Click(object sender, EventArgs e)
         {
             OleDbCommand cmd =
-               new OleDbCommand("SELECT 仕入先ID , 仕入先会社名 ,仕入先郵便番号 , 仕入先住所 ," +
-                "仕入先電話番号 , 商品ID ," +
-                "FROM 仕入先マスタ WHERE 仕入先郵便番号 = @仕入先郵便番号  ORDER BY 仕入先郵便番号");
+              new OleDbCommand(("SELECT 仕入先ID ,商品ID ,仕入先会社名 ,仕入先郵便番号 ,仕入先住所 ,仕入先電話番号  " +
+              "FROM 仕入先マスタ WHERE 仕入先郵便番号  = @仕入先郵便番号 ORDER BY 仕入先郵便番号 "));
             cmd.Connection = cn;
             OleDbDataAdapter da = new OleDbDataAdapter();
             da.SelectCommand = cmd;
 
-            cmd.Parameters.AddWithValue("@仕入先郵便番号", cmb_postalcode.Text);
+            cmd.Parameters.AddWithValue("@仕入先郵便番号 ", txt_postalcode.Text);
 
 
             DataTable dt = new DataTable();
@@ -149,14 +89,13 @@ namespace SystemDev_KY_22.ユーザーコントロール
         private void btn_telsearch_Click(object sender, EventArgs e)
         {
             OleDbCommand cmd =
-               new OleDbCommand("SELECT 仕入先ID , 仕入先会社名 ,仕入先郵便番号 , 仕入先住所 ," +
-                "仕入先電話番号 , 商品ID ," +
-                "FROM 仕入先マスタ WHERE 仕入先電話番号 = @仕入先電話番号  ORDER BY 仕入先電話番号");
+              new OleDbCommand("SELECT 仕入先ID ,商品ID ,仕入先会社名 ,仕入先郵便番号 ,仕入先住所 ,仕入先電話番号  " +
+               "FROM 仕入先マスタ WHERE 仕入先電話番号 = @仕入先電話番号 ORDER BY 仕入先電話番号");
             cmd.Connection = cn;
             OleDbDataAdapter da = new OleDbDataAdapter();
             da.SelectCommand = cmd;
 
-            cmd.Parameters.AddWithValue("@仕入先電話番号", cmb_tel.Text);
+            cmd.Parameters.AddWithValue("@仕入先電話番号", txt_tel.Text);
 
 
             DataTable dt = new DataTable();
@@ -169,14 +108,13 @@ namespace SystemDev_KY_22.ユーザーコントロール
         private void btn_addresssearch_Click(object sender, EventArgs e)
         {
             OleDbCommand cmd =
-               new OleDbCommand("SELECT 仕入先ID , 仕入先会社名 ,仕入先郵便番号 , 仕入先住所 ," +
-                "仕入先電話番号 , 商品ID ," +
-                "FROM 仕入先マスタ WHERE 仕入先住所 = @仕入先住所  ORDER BY 仕入先城所");
+              new OleDbCommand("SELECT 仕入先ID ,商品ID ,仕入先会社名 ,仕入先郵便番号 ,仕入先住所 ,仕入先電話番号 " +
+              "FROM 仕入先マスタ WHERE 仕入先住所 = @仕入先住所 ORDER BY 仕入先住所");
             cmd.Connection = cn;
             OleDbDataAdapter da = new OleDbDataAdapter();
             da.SelectCommand = cmd;
 
-            cmd.Parameters.AddWithValue("@仕入先住所", cmb_address.Text);
+            cmd.Parameters.AddWithValue("@仕入先住所", txt_address.Text);
 
 
             DataTable dt = new DataTable();
@@ -189,14 +127,13 @@ namespace SystemDev_KY_22.ユーザーコントロール
         private void btn_productsearch_Click(object sender, EventArgs e)
         {
             OleDbCommand cmd =
-               new OleDbCommand("SELECT 仕入先ID , 仕入先会社名 ,仕入先郵便番号 , 仕入先住所 ," +
-                "仕入先電話番号 , 商品ID ," +
-                "FROM 仕入先マスタ WHERE 商品ID = @商品ID  ORDER BY 商品ID");
+             new OleDbCommand("SELECT 商品ID ,車種ID ,商品名 ,メーカー ,色 ,モデル ,オプション ,発売日 ,走行距離 ,定価 ,実売価格 " +
+              "FROM 商品マスタ WHERE 商品ID = @商品ID ORDER BY 商品ID");
             cmd.Connection = cn;
             OleDbDataAdapter da = new OleDbDataAdapter();
             da.SelectCommand = cmd;
 
-            cmd.Parameters.AddWithValue("@商品ID", cmb_productID.Text);
+            cmd.Parameters.AddWithValue("@商品ID", txt_productID.Text);
 
 
             DataTable dt = new DataTable();
@@ -210,17 +147,18 @@ namespace SystemDev_KY_22.ユーザーコントロール
         {
             // ID、Pass、Name、PostNumber、AddressをINSERT  
             OleDbCommand cmd =
-                new OleDbCommand("INSERT INTO 仕入先マスタ (仕入先ID,仕入先会社名, 仕入先郵便番号,仕入先住所, 仕入先電話番号,商品ID) " +
-                "VALUES (@仕入先ID, @仕入先会社名, @仕入先郵便番号, @仕入先住所, @商品ID)", cn);
+                new OleDbCommand("INSERT INTO 仕入先マスタ (仕入先ID, 商品ID, 仕入先会社名, 仕入先郵便番号, 仕入先住所, 仕入先電話番号) " +
+                "VALUES (@仕入先ID,@商品ID, @仕入先会社名, @仕入先郵便番号,@仕入先電話番号, @仕入先住所)", cn);
             //DBの列名に、PassWord (Microsoft Jet 4.0 の予約語)は使用できない
             //@パラメータが出てくる順番に設定する
-            cmd.Parameters.AddWithValue("@仕入先ID", cmb_SupplierID);
-            cmd.Parameters.AddWithValue("@仕入先会社名", cmb_Suppliername);                 //IDのデータ
-            cmd.Parameters.AddWithValue("@仕入先郵便番号", cmb_postalcode);             //Passのデータ
-            cmd.Parameters.AddWithValue("@仕入先住所", cmb_tel);             //Nameのデータ
-            cmd.Parameters.AddWithValue("@仕入先電話番号", cmb_address.Text);  //PostNumberのデータ
-            cmd.Parameters.AddWithValue("@商品ID", cmb_productID.Text); 
+            cmd.Parameters.AddWithValue("@仕入先ID", txt_SupplierID.Text);
+            cmd.Parameters.AddWithValue("@商品ID", txt_productID.Text);
+            cmd.Parameters.AddWithValue("@仕入先会社名", txt_Suppliername.Text);                 //IDのデータ
+            cmd.Parameters.AddWithValue("@仕入先郵便番号", txt_postalcode.Text);             //Passのデータ
+            cmd.Parameters.AddWithValue("@仕入先住所", txt_address.Text);             //Nameのデータ
+            cmd.Parameters.AddWithValue("@仕入先電話番号", txt_tel.Text);  //PostNumberのデータ
             
+
             try
             {
                 cn.Open();                 //コネクションを開く
@@ -240,19 +178,23 @@ namespace SystemDev_KY_22.ユーザーコントロール
 
         private void btn_update_Click(object sender, EventArgs e)
         {
+            //OleDbCommand cmd =
+            //          new OleDbCommand("UPDATE 仕入先マスタ SET 仕入先ID = @SupplierID , 仕入先会社名 = @Suppliername , " +
+            //           "仕入先郵便番号 = @Supplierpostal ,仕入先住所 = @Supplieraddress ,仕入先電話番号 = @Suppliertel ,商品ID = @productID WHERE (仕入先ID = @仕入先ID)", cn);
             OleDbCommand cmd =
-                       new OleDbCommand("UPDATE 仕入先マスタ SET 仕入先ID = @SupplierID,仕入先会社名 = @Suppliername, " +
-                        " 仕入先郵便番号 = @Supplierpostal,仕入先住所 = @Supplieraddress,仕入先電話番号 = @Suppliertell,商品ID = @productID, WHERE (仕入先ID = @仕入先ID)", cn);
+                     new OleDbCommand("UPDATE 仕入先マスタ SET 商品ID = @productID ,仕入先会社名 = @Suppliername , " +
+                      "仕入先郵便番号 = @Supplierpostal ,仕入先住所 = @Supplieraddress ,仕入先電話番号 = @Suppliertel WHERE (仕入先ID = @SupplierID)", cn);
             //@パラメータが出てくる順番に設定する
 
 
-            cmd.Parameters.AddWithValue("@SupplierID", cmb_SupplierID);
-            cmd.Parameters.AddWithValue(" @Suppliername", cmb_Suppliername.Text);               //Passのデータ
-            cmd.Parameters.AddWithValue("@Supplierpostal", cmb_postalcode.Text);               //Nameのデータ
-            cmd.Parameters.AddWithValue("@Supplieraddress", cmb_tel.Text);     //PostNumberのデータ
-            cmd.Parameters.AddWithValue("@Suppliertell", cmb_address.Text);           //Addressのデータ
-            cmd.Parameters.AddWithValue("@productID", cmb_productID.Text);//IDのデータ
+            cmd.Parameters.AddWithValue("@SupplierID", txt_SupplierID.Text);
+            cmd.Parameters.AddWithValue("@productID", txt_productID.Text);//IDのデータ
+            cmd.Parameters.AddWithValue("@Suppliername", txt_Suppliername.Text);               //Passのデータ
+            cmd.Parameters.AddWithValue("@Supplierpostal", txt_postalcode.Text);               //Nameのデータ
+            cmd.Parameters.AddWithValue("@Supplieraddress", txt_address.Text);     //PostNumberのデータ
+            cmd.Parameters.AddWithValue("@Suppliertel", txt_tel.Text);           //Addressのデータ
             
+
 
             try
             {
@@ -271,8 +213,14 @@ namespace SystemDev_KY_22.ユーザーコントロール
 
         private void btn_Clear_Click(object sender, EventArgs e)
         {
-            
+            txt_SupplierID.Clear();
+            txt_Suppliername.Clear();
+            txt_postalcode.Clear();
+            txt_tel.Clear();
+            txt_tel.Clear();
+            txt_address.Clear();
+            txt_productID.Clear();
+
         }
     }
 }
-
