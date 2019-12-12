e805cd5bc11c106f060d72988929c013bc80a14f
maru8375 <55867559+maru8375@users.noreply.github.com>
2019-12-12 16:13:02 +0900

    在庫一覧　完成

--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/StockList.Designer.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/StockList.Designer.cs
@@ -35,7 +35,6 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -49,19 +48,26 @@
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label4 = new System.Windows.Forms.Label();
+            this.btn_CarNameSeach = new System.Windows.Forms.Button();
+            this.btn_MakerSeach = new System.Windows.Forms.Button();
+            this.btn_PriceSeach = new System.Windows.Forms.Button();
+            this.btn_MileageSeach = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btn_MileageSeach);
+            this.panel1.Controls.Add(this.btn_PriceSeach);
+            this.panel1.Controls.Add(this.btn_MakerSeach);
+            this.panel1.Controls.Add(this.btn_CarNameSeach);
             this.panel1.Controls.Add(this.checkBox1);
             this.panel1.Controls.Add(this.comboBox5);
             this.panel1.Controls.Add(this.comboB_CarName);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.comboBox4);
-            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.comboBox1);
@@ -81,38 +87,49 @@
             this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.checkBox1.Location = new System.Drawing.Point(891, 78);
+            this.checkBox1.Location = new System.Drawing.Point(859, 69);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(130, 40);
+            this.checkBox1.Size = new System.Drawing.Size(162, 49);
             this.checkBox1.TabIndex = 31;
             this.checkBox1.Text = "在庫なし";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // comboBox5
             // 
             this.comboBox5.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(679, 75);
+            this.comboBox5.Items.AddRange(new object[] {
+            "10000",
+            "20000",
+            "30000",
+            "40000",
+            "50000",
+            "100000",
+            "150000",
+            "200000",
+            "99999999"});
+            this.comboBox5.Location = new System.Drawing.Point(769, 76);
             this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 35);
+            this.comboBox5.Size = new System.Drawing.Size(121, 42);
             this.comboBox5.TabIndex = 28;
             // 
             // comboB_CarName
             // 
             this.comboB_CarName.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboB_CarName.FormattingEnabled = true;
-            this.comboB_CarName.Location = new System.Drawing.Point(180, 27);
+            this.comboB_CarName.Location = new System.Drawing.Point(177, 31);
             this.comboB_CarName.Name = "comboB_CarName";
-            this.comboB_CarName.Size = new System.Drawing.Size(121, 35);
+            this.comboB_CarName.Size = new System.Drawing.Size(121, 42);
             this.comboB_CarName.TabIndex = 29;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label7.Location = new System.Drawing.Point(613, 80);
+            this.label7.Location = new System.Drawing.Point(703, 81);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(34, 24);
+            this.label7.Size = new System.Drawing.Size(43, 30);
             this.label7.TabIndex = 27;
             this.label7.Text = "～";
             // 
@@ -122,7 +139,7 @@
             this.label8.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label8.Location = new System.Drawing.Point(68, 29);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(63, 36);
+            this.label8.Size = new System.Drawing.Size(80, 45);
             this.label8.TabIndex = 30;
             this.label8.Text = "車名";
             // 
@@ -130,31 +147,28 @@
             // 
             this.comboBox4.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(461, 74);
+            this.comboBox4.Items.AddRange(new object[] {
+            "0",
+            "10001",
+            "20001",
+            "30001",
+            "40001",
+            "50001",
+            "100001",
+            "150001",
+            "200001"});
+            this.comboBox4.Location = new System.Drawing.Point(551, 75);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 35);
+            this.comboBox4.Size = new System.Drawing.Size(121, 42);
             this.comboBox4.TabIndex = 26;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(815, 15);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 45);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "検索";
-            this.button1.UseVisualStyleBackColor = false;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label6.Location = new System.Drawing.Point(327, 76);
+            this.label6.Location = new System.Drawing.Point(417, 77);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(111, 36);
+            this.label6.Size = new System.Drawing.Size(140, 45);
             this.label6.TabIndex = 25;
             this.label6.Text = "走行距離";
             // 
@@ -164,20 +178,37 @@
             this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button2.Location = new System.Drawing.Point(921, 16);
+            this.button2.Location = new System.Drawing.Point(885, 16);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 45);
+            this.button2.Size = new System.Drawing.Size(136, 45);
             this.button2.TabIndex = 17;
             this.button2.Text = "更新";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(679, 28);
+            this.comboBox1.Items.AddRange(new object[] {
+            "20000",
+            "30000",
+            "40000",
+            "50000",
+            "60000",
+            "70000",
+            "80000",
+            "90000",
+            "100000",
+            "150000",
+            "200000",
+            "300000",
+            "400000",
+            "500000",
+            "99999999"});
+            this.comboBox1.Location = new System.Drawing.Point(769, 29);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 35);
+            this.comboBox1.Size = new System.Drawing.Size(121, 42);
             this.comboBox1.TabIndex = 24;
             // 
             // label3
@@ -186,7 +217,7 @@
             this.label3.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label3.Location = new System.Drawing.Point(34, 71);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 36);
+            this.label3.Size = new System.Drawing.Size(140, 45);
             this.label3.TabIndex = 13;
             this.label3.Text = "メーカー";
             // 
@@ -194,9 +225,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(613, 32);
+            this.label1.Location = new System.Drawing.Point(703, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 24);
+            this.label1.Size = new System.Drawing.Size(43, 30);
             this.label1.TabIndex = 20;
             this.label1.Text = "～";
             // 
@@ -204,27 +235,43 @@
             // 
             this.comboBox3.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(461, 27);
+            this.comboBox3.Items.AddRange(new object[] {
+            "10000",
+            "20001",
+            "30001",
+            "40001",
+            "50001",
+            "60001",
+            "70001",
+            "80001",
+            "90001",
+            "100001",
+            "150001",
+            "200001",
+            "300001",
+            "400001",
+            "500001"});
+            this.comboBox3.Location = new System.Drawing.Point(551, 27);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 35);
+            this.comboBox3.Size = new System.Drawing.Size(121, 42);
             this.comboBox3.TabIndex = 23;
             // 
             // comboB_Maker
             // 
             this.comboB_Maker.Font = new System.Drawing.Font("メイリオ", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.comboB_Maker.FormattingEnabled = true;
-            this.comboB_Maker.Location = new System.Drawing.Point(180, 74);
+            this.comboB_Maker.Location = new System.Drawing.Point(177, 73);
             this.comboB_Maker.Name = "comboB_Maker";
-            this.comboB_Maker.Size = new System.Drawing.Size(121, 35);
+            this.comboB_Maker.Size = new System.Drawing.Size(121, 42);
             this.comboB_Maker.TabIndex = 22;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label5.Location = new System.Drawing.Point(348, 28);
+            this.label5.Location = new System.Drawing.Point(438, 29);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(63, 36);
+            this.label5.Size = new System.Drawing.Size(80, 45);
             this.label5.TabIndex = 15;
             this.label5.Text = "価格";
             // 
@@ -258,6 +305,7 @@
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(19, 149);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 21;
             this.dataGridView1.Size = new System.Drawing.Size(1002, 527);
             this.dataGridView1.TabIndex = 5;
@@ -267,8 +315,64 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(109, 202);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.Size = new System.Drawing.Size(0, 15);
             this.label4.TabIndex = 14;
+            // 
+            // btn_CarNameSeach
+            // 
+            this.btn_CarNameSeach.BackColor = System.Drawing.Color.White;
+            this.btn_CarNameSeach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_CarNameSeach.Font = new System.Drawing.Font("MS UI Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_CarNameSeach.Location = new System.Drawing.Point(307, 29);
+            this.btn_CarNameSeach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_CarNameSeach.Name = "btn_CarNameSeach";
+            this.btn_CarNameSeach.Size = new System.Drawing.Size(45, 45);
+            this.btn_CarNameSeach.TabIndex = 32;
+            this.btn_CarNameSeach.Text = "🔍";
+            this.btn_CarNameSeach.UseVisualStyleBackColor = false;
+            this.btn_CarNameSeach.Click += new System.EventHandler(this.btn_CarNameSeach_Click);
+            // 
+            // btn_MakerSeach
+            // 
+            this.btn_MakerSeach.BackColor = System.Drawing.Color.White;
+            this.btn_MakerSeach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_MakerSeach.Font = new System.Drawing.Font("MS UI Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_MakerSeach.Location = new System.Drawing.Point(307, 76);
+            this.btn_MakerSeach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_MakerSeach.Name = "btn_MakerSeach";
+            this.btn_MakerSeach.Size = new System.Drawing.Size(45, 45);
+            this.btn_MakerSeach.TabIndex = 33;
+            this.btn_MakerSeach.Text = "🔍";
+            this.btn_MakerSeach.UseVisualStyleBackColor = false;
+            this.btn_MakerSeach.Click += new System.EventHandler(this.btn_MakerSeach_Click);
+            // 
+            // btn_PriceSeach
+            // 
+            this.btn_PriceSeach.BackColor = System.Drawing.Color.White;
+            this.btn_PriceSeach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_PriceSeach.Font = new System.Drawing.Font("MS UI Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_PriceSeach.Location = new System.Drawing.Point(896, 25);
+            this.btn_PriceSeach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_PriceSeach.Name = "btn_PriceSeach";
+            this.btn_PriceSeach.Size = new System.Drawing.Size(45, 45);
+            this.btn_PriceSeach.TabIndex = 34;
+            this.btn_PriceSeach.Text = "🔍";
+            this.btn_PriceSeach.UseVisualStyleBackColor = false;
+            this.btn_PriceSeach.Click += new System.EventHandler(this.btn_PriceSeach_Click);
+            // 
+            // btn_MileageSeach
+            // 
+            this.btn_MileageSeach.BackColor = System.Drawing.Color.White;
+            this.btn_MileageSeach.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_MileageSeach.Font = new System.Drawing.Font("MS UI Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_MileageSeach.Location = new System.Drawing.Point(896, 75);
+            this.btn_MileageSeach.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_MileageSeach.Name = "btn_MileageSeach";
+            this.btn_MileageSeach.Size = new System.Drawing.Size(45, 45);
+            this.btn_MileageSeach.TabIndex = 35;
+            this.btn_MileageSeach.Text = "🔍";
+            this.btn_MileageSeach.UseVisualStyleBackColor = false;
+            this.btn_MileageSeach.Click += new System.EventHandler(this.btn_MileageSeach_Click);
             // 
             // StockList
             // 
@@ -300,7 +404,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboB_Maker;
@@ -313,5 +416,9 @@
         private System.Windows.Forms.ComboBox comboB_CarName;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btn_CarNameSeach;
+        private System.Windows.Forms.Button btn_MakerSeach;
+        private System.Windows.Forms.Button btn_MileageSeach;
+        private System.Windows.Forms.Button btn_PriceSeach;
     }
 }
