bbd2dd5bb6bd94ee2aff11e099c7270fc5349a82
Keigo0427 <55825640+Keigo0427@users.noreply.github.com>
2019-12-13 17:22:26 +0900

    受注履歴等の作成

--- OrderHistory/OrderHistory/Program.cs
+++ OrderHistory/OrderHistory/Program.cs
@@ -19,14 +19,21 @@ namespace OrderHistory
                 @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\SysDev.accdb;";
 
             // タイマーの間隔(ミリ秒)
+            //Timer timer = new Timer(300000);
             Timer timer = new Timer(300000);
 
+            int a;
+            int b;
+            int c;
 
             // タイマーを開始する
             timer.Start();
-
+            
             timer.Elapsed += (sender, e) =>
             {
+                DateTime time = DateTime.Now;
+                Console.WriteLine("System:テーブルの更新状況の確認を開始します("+time+")");
+
                 OleDbCommand cmd = new OleDbCommand();
                 cmd.Connection = cn;
                 cn.Open();           //先にDBを開いておく
@@ -41,26 +48,31 @@ namespace OrderHistory
                 DataTable dt2 = new DataTable();
                 da2.Fill(dt2);
 
-                if (int.Parse(dt.Rows[dt.Rows.Count - 1][0].ToString()) > int.Parse(dt2.Rows[dt.Rows.Count - 1][1].ToString()))
-                {
-                    int load = int.Parse(dt.Rows[dt.Rows.Count - 1][0].ToString());
+                a = int.Parse(dt.Rows[dt.Rows.Count - 1]["注文ID"].ToString());
+                b = int.Parse(dt2.Rows[dt2.Rows.Count - 1]["注文ID"].ToString());
 
-                    for (int unload = int.Parse(dt2.Rows[dt.Rows.Count - 1][1].ToString()); unload < load; unload++)
+                cn.Close();
+                
+                if (a > b)
+                {
+                    Console.WriteLine("System:受注リストにない新しい注文データが見つかりました。");
+                    Console.WriteLine("System:登録を開始します。" + Environment.NewLine);
+                    cn.Open();
+                    for (b++; b <= a; b++)
                     {
-                        // ID、Pass、Name、PostNumber、AddressをINSERT  
+
+                        da2.Fill(dt2);
+                        c = int.Parse(dt2.Rows[dt2.Rows.Count - 1]["受注ID"].ToString()) + 1;
+           
                         OleDbCommand cmd3 =
                             new OleDbCommand("INSERT INTO 受注テーブル (受注ID,注文ID) " +
                             "VALUES (@受注ID, @注文ID)", cn);
-                        //DBの列名に、PassWord (Microsoft Jet 4.0 の予約語)は使用できない
-                        //@パラメータが出てくる順番に設定する
-                        cmd3.Parameters.AddWithValue("@受注ID", (int.Parse(dt2.Rows[unload][0].ToString() + 1).ToString()));
-                        cmd3.Parameters.AddWithValue("@注文ID", (int.Parse(dt.Rows[load][0].ToString() + 1).ToString()));
-                        load++;//IDのデータ
+                        cmd3.Parameters.AddWithValue("@受注ID", c.ToString());
+                        cmd3.Parameters.AddWithValue("@注文ID", b.ToString());
+
                         try
                         {
-                            cn.Open();                 //コネクションを開く
-                            cmd3.ExecuteNonQuery();     //SQLを実行
-                            cn.Close();                //コネクションを閉じる
+                            cmd3.ExecuteNonQuery();
                         }
                         catch (Exception ex)
                         {
@@ -68,21 +80,17 @@ namespace OrderHistory
                             cn.Close();               //コネクションを閉じる
                             return;
                         }
-
+                        Console.WriteLine("System:新たに登録されたデータ -> 受注ID:" + c.ToString() + " 注文ID:" + b.ToString());
                     }
-
-
-                    Console.WriteLine("注文テーブルの変更があったので登録しました");
+                    Console.WriteLine(Environment.NewLine + "System:データの更新が完了しました" + Environment.NewLine + Environment.NewLine);
+                    cn.Close();
                 }
-
-
-
-                cn.Close();
+                else
+                {
+                    Console.WriteLine("System:新しいデータはありませんでした" + Environment.NewLine + Environment.NewLine);
+                }
             };
-
             Console.ReadKey();
         }
-
-
     }
 }
--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/OrderCheck.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/OrderCheck.cs
@@ -7,14 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace SystemDev_KY_22.ユーザーコントロール
 {
     public partial class OrderCheck : UserControl
     {
+        OleDbConnection cn;  //コネクションオブジェクト
+
         public OrderCheck()
         {
             InitializeComponent();
+        }
+
+        private void OrderCheck_Load(object sender, EventArgs e)
+        {
+            if (!this.DesignMode)
+            {
+                cn = new OleDbConnection();
+                cn.ConnectionString =
+                    @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\SysDev.accdb;";
+                dataload();
+            }
+        }
+
+        private void dataload()
+        {
+            OleDbDataAdapter da =
+                new OleDbDataAdapter("SELECT 受注テーブル.受注ID, 受注テーブル.注文ID, 注文テーブル.数量, 注文テーブル.商品詳細ID, 注文テーブル.在庫減フラグ, 注文テーブル.店舗ID, 注文テーブル.注文日, 受注テーブル.処理 FROM 受注テーブル INNER JOIN 注文テーブル ON 受注テーブル.受注ID = 注文テーブル.注文ID", cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgv_ordercheck.DataSource = dt;
+            dgv_ordercheck.AllowUserToAddRows = false;    //最下行を非表示
+            dgv_ordercheck.AutoResizeColumns();           //列の幅の自動調整
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            OleDbCommand cmd =
+                   new OleDbCommand("SELECT 顧客ID , 氏名 , 住所 , 郵便番号 , 電話番号 , 性別 , 生年月日 FROM 顧客マスタ WHERE 顧客ID = @顧客ID ORDER BY 顧客ID", cn);
+            cmd.Connection = cn;
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            da.SelectCommand = cmd;
+
+            cmd.Parameters.AddWithValue("@受注ID", mxt_productname.Text);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            mxt_productname.ResetText();
+            dtp_orderdate1.ResetText();
+            dtp_orderdate2.ResetText();
+        }
+
+        private void dgv_ordercheck_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
