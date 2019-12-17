9a5064c1e7e1e7c0ba912b03c10446c8d5930e84
yukke396 <55867870+yukke396@users.noreply.github.com>
2019-12-17 17:52:28 +0900

    注文画面の項目を検索結果をTextboxに反映出来てデータベースに追加と連番がまだ

--- SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/Order.cs
+++ SystemDev_KY-22/SystemDev_KY-22/ユーザーコントロール/Order.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.Data.SqlClient;
 
 namespace SystemDev_KY_22
 {
@@ -43,40 +44,14 @@ namespace SystemDev_KY_22
             comboBox1.ValueMember = "店舗ID";
 
 
-            //OleDbDataAdapter cmb_manu_da =
-            //    new OleDbDataAdapter("SELECT メーカー FROM メーカーマスタ", cn);
-            //DataTable manufacturer_dt = new DataTable();
-            //cmb_manu_da.Fill(manufacturer_dt);
-
-            //cmb_manufacturer.DataSource = manufacturer_dt;
-            //cmb_manufacturer.DisplayMember = "メーカー";
-            //cmb_manufacturer.ValueMember = "メーカー";
-
-
-            //OleDbDataAdapter cmb2_da =
-            //    new OleDbDataAdapter("SELECT 車種,安全在庫数 FROM 車種マスタ", cn);
-            //DataTable cmb2_dt = new DataTable();
-            //cmb2_da.Fill(cmb2_dt);
-
-            //comboBox2.DataSource = cmb2_dt;
-            //comboBox2.DisplayMember = "車種";
-            //comboBox2.ValueMember = "車種";
-
-            //OleDbDataAdapter txt_money_da =
-            //    new OleDbDataAdapter("SELECT 商品ID,商品名,定価,車種,メーカー,仕入れ先ID FROM 商品マスタ", cn);
-            //DataTable amountofmoney_dt = new DataTable();
-            //txt_money_da.Fill(amountofmoney_dt);
-
-            //txt_amountofmoney.DataSource = amountofmoney_dt;
-            //txt_amountofmoney.DisplayMember = "定価";
-            //txt_amountofmoney.ValueMember = "定価";
+            
 
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+
             cmb_manufacturer.SelectedIndex = -1;
             comboBox2.SelectedIndex = -1;
             textBox2.Clear();
@@ -87,7 +62,8 @@ namespace SystemDev_KY_22
             txt_employeeID.Clear();
             txt_clientID.Clear();
             txt_amountofmoney.Clear();
-
+            textBox3.Clear();
+            txt_itemid.Clear();
 
             dataload();
 
@@ -95,48 +71,59 @@ namespace SystemDev_KY_22
 
         private void btn_decision_Click(object sender, EventArgs e)
         {
+
+            string queryString = "SELECT 注文ID FROM 注文テーブル ORDER BY 注文ID DESC";
+            OleDbDataAdapter da = new OleDbDataAdapter(queryString, cn);
+            DataTable dt = new DataTable();
+
+            DataRow dr = dt.Rows[0];
+
+            da.Fill(dt);
+            orederid = int.Parse(dr["注文ID"].ToString());
+            orederid += 1;
+
             OleDbCommand cmd =
-               new OleDbCommand("INSERT INTO 注文テーブル (注文ID ,数量 ,顧客ID ,社員ID ,商品詳細ID," +
-                                " 発送日 , 在庫減フラグ , 店舗ID , 注文日)" +
-               "VALUES (@注文ID,@数量,@顧客ID,@社員ID,@商品詳細ID,@発送日,@在庫減フラグ,@店舗ID,@注文日)", cn);
+               new OleDbCommand("INSERT INTO 注文テーブル (注文ID ,数量 ,顧客ID ,社員ID ,商品詳細ID" +
+                                " 発送日 , 在庫減フラグ , 店舗ID , 受注日)" +
+               "VALUES (@注文ID,@数量,@顧客ID,@社員ID,@商品詳細ID,@発送日,@在庫減フラグ,@店舗ID,@受注日)", cn);
+
+           /* string queryString = "SELECT 注文ID FROM 注文テーブル ORDER BY 注文ID DESC";
+            OleDbDataAdapter da = new OleDbDataAdapter(queryString, cn);
+            DataTable dt = new DataTable();
+
+            DataRow dr = dt.Rows[0];
+
+            da.Fill(dt);
+            orederid = int.Parse(dr["注文ID"].ToString());
+            orederid += 1;*/
+
 
             //@パラメータが出てくる順番に設定する
+            cmd.Parameters.AddWithValue("@注文ID", orederid.ToString());
+            cmd.Parameters.AddWithValue("@受注日", dtp_Orderdate.Text);
             cmd.Parameters.AddWithValue("@店舗ID", txt_clientID.Text);
             cmd.Parameters.AddWithValue("@社員ID", txt_employeeID.Text);
             cmd.Parameters.AddWithValue("@顧客ID", comboBox1.Text);
             cmd.Parameters.AddWithValue("@数量", textBox3.Text);
 
-            MessageBox.Show("登録しますか",
-                            "登録",
-                             MessageBoxButtons.OK,
-                             MessageBoxIcon.Error);
-            //string queryString = "SELECT 注文ID FROM 注文テーブル ORDER BY 注文ID DESC";
-            //OleDbDataAdapter da = new OleDbDataAdapter(queryString,cn);
-            //DataTable dt = new DataTable();
 
-            //DataRow dr = dt.Rows[0];
-
-            //da.Fill(dt);
-            //orederid = int.Parse(dr["注文ID"].ToString());
-            //orederid += 1;
 
 
         }
         private void button2_Click(object sender, EventArgs e)
         {
             OleDbCommand cmd =
-              new OleDbCommand("SELECT 商品詳細テーブル.商品詳細ID, 商品マスタ.[メーカー], 商品マスタ.車種, 商品マスタ.商品名, 商品詳細テーブル.色, 商品詳細テーブル.車両状態, 商品詳細テーブル.実売価格, 商品詳細テーブル.[オプション] " +
-              "FROM 商品マスタ INNER JOIN 商品詳細テーブル ON 商品マスタ.商品ID = 商品詳細テーブル.商品ID WHERE 商品詳細テーブル.商品詳細ID = @商品詳細ID");
+              new OleDbCommand("SELECT 商品詳細テーブル.商品詳細ID, 商品マスタ.[メーカー], 商品マスタ.車種, 商品マスタ.商品名, 商品詳細テーブル.色, 商品詳細テーブル.車両状態, 商品詳細テーブル.実売価格, 商品詳細テーブル.[オプション]"+
+                                "FROM 商品マスタ INNER JOIN 商品詳細テーブル ON 商品マスタ.商品ID = 商品詳細テーブル.商品ID",cn);
+
 
             cmd.Connection = cn;
             OleDbDataAdapter da = new OleDbDataAdapter();
-            cmd.Parameters.AddWithValue("@商品詳細ID", txt_itemid.Text);
             da.SelectCommand = cmd;
-
-            da.Fill(dt);
+            cmd.Parameters.AddWithValue("@商品詳細ID", txt_itemid.Text);
             try
             {
-                cn.Open();
+//              cn.Open();
 
                 DataTable dt = new DataTable();
                 try
@@ -144,6 +131,10 @@ namespace SystemDev_KY_22
                     da.Fill(dt);
                     DataRow dr = dt.Rows[0];
                     cmb_manufacturer.Text = dr["メーカー"].ToString();
+                    comboBox2.Text = dr["車種"].ToString();
+                    textBox2.Text = dr["商品名"].ToString();
+                    textBox1.Text=dr["色"].ToString();
+                    txt_amountofmoney.Text = dr["実売価格"].ToString();
                     cn.Close();
                 }
                 catch (Exception ex)
@@ -155,29 +146,21 @@ namespace SystemDev_KY_22
             }
             catch (Exception ex)
             {
-                MessageBox.Show("登録できません。"+ ex.ToString());
+                MessageBox.Show("登録できません。" + ex.ToString());
                 cn.Close();                //コネクションを閉じる
                 return;
             }
 
-         }
-
-
-        
-
-
         }
-        //private void txt_amountofmoney_TextChanged(object sender, EventArgs e)
-        //{
-        //    txt_consumptiontax.Text = (int.Parse(txt_amountofmoney.Text) * 0.10).ToString();
-        //    txt_total.Text = (int.Parse(txt_amountofmoney.Text) * 1.10).ToString();
-        //}
 
-        //private void textBox4_TextChanged(object sender, EventArgs e)
-        //{
 
-        //}
+        private void txt_amountofmoney_TextChanged_1(object sender, EventArgs e)
+        {
+            txt_consumptiontax.Text = (int.Parse(txt_amountofmoney.Text) * 0.10).ToString();
+            txt_total.Text = (int.Parse(txt_amountofmoney.Text) * 1.10).ToString();
+        }
     }
+}
     
             
     
